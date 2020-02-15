c64de729f664991e51bc8c4692b3e94e84367102
gammafer <60357762+gammafer@users.noreply.github.com>
2020-02-15 18:22:07 +0100

    New build files

---
+++ precache-manifest.0fe2771ddddb36748bbc09907384fc7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be76b52f1b582773fcf48ca6d9df77a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d55b85f18480d3705ef1",
+    "url": "./static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9b0a05374c06b075a325",
+    "url": "./static/css/main.6880a1c7.chunk.css"
+  },
+  {
+    "revision": "d55b85f18480d3705ef1",
+    "url": "./static/js/2.91a8bb70.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.91a8bb70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b0a05374c06b075a325",
+    "url": "./static/js/main.0ea6a976.chunk.js"
+  },
+  {
+    "revision": "da82ad3ab93de7342fe4",
+    "url": "./static/js/runtime-main.7575b914.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "./static/media/SEGA.12a3db8b.woff"
+  }
+]);
